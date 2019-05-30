1eb55dbd5f4035c1a89a11ac70d0d6c7f8a850b6
Gabriel Martins - GMR <gabriel.martins@iteris.com.br>
2019-05-30 12:40:08 -0300

    estilo mudado e imagem implementada

--- src/components/cabecalho/index.js
+++ src/components/cabecalho/index.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react'
+import eye from "../../images/eye.png"
 import './cabecalho.css'
 import './navMenu.css'
 
 class Cabecalho extends Component {
     render() {
         return (
-            <header className="cabecalho">
-                <div className="cabecalho__container	container">
-                    <h1 className="cabecalho__logo">
-                        <a href="/">Twitter</a>
-                    </h1>
-                    <nav className="navMenu">
-                        <ul className="navMenu__lista">
-                            <li className="navMenu__item">
-                                <a className="navMenu__link" href="/">
-                                    Bem	vindo(a):	<br />
-                                    <strong>{this.props.usuario}</strong>
-                                </a>
-                            </li>
-                            <li className="navMenu__item">
-                                <a className="navMenu__link" href="/">Página Inicial</a>
-                            </li>
-                            <li className="navMenu__item">
-                                <a className="navMenu__link" href="/hashtags">Hashtags</a>
-                            </li>
-                            <li className="navMenu__item">
-                                <a className="navMenu__link" href="/logout">Logout</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+            <body>
+                <header className="cabecalho">
+                    <div className="cabecalho__container	container">
+                        <h1 className="cabecalho__logo">
+                            <a href="/">Twitter</a>
+                        </h1>
+                        <img src={eye} alt="Eye" height="80" width="80"></img>
+                        <nav className="navMenu">
+                            <ul className="navMenu__lista">
+                                <li className="navMenu__item">
+                                    <a className="navMenu__link" href="/">
+                                        Bem	vindo(a):	<br />
+                                        <strong>{this.props.usuario}</strong>
+                                    </a>
+                                </li>
+                                <li className="navMenu__item">
+                                    <a className="navMenu__link" href="/">Página Inicial</a>
+                                </li>
+                                <li className="navMenu__item">
+                                    <a className="navMenu__link" href="/hashtags">Hashtags</a>
+                                </li>
+                                <li className="navMenu__item">
+                                    <a className="navMenu__link" href="/logout">Logout</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+            </body>
         )
     }
 }
